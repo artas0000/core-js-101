ebe1697f7ed26c664878fcc80b6881d172cdf122
artas0000 <anisovich28@mail.ru>
2019-12-21 00:03:10 +0300

    feat: add test solution

--- src/01-strings-tasks.js
+++ src/01-strings-tasks.js
@@ -282,8 +282,11 @@ function isString(value) {
  *   'Q♠' => 50
  *   'K♠' => 51
  */
-function getCardId(/* value */) {
-  throw new Error('Not implemented');
+function getCardId(value) {
+  const ranks = ['A', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K'];
+  const suits = ['♣', '♦', '♥', '♠'];
+
+  return ranks.indexOf(value.slice(0, -1)) + suits.indexOf(value.slice(-1)) * 13;
 }
 
 
